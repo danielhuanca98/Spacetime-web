3c52f0d83d7ec5c4a7a71aded150cccfca3256bd
Daniel Huanca <daniel46758@gmail.com>
2023-05-25 20:51:28 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', '192.168.15.2'],
+  },
+}
+
+module.exports = nextConfig
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export default function Profile() {
+  const { name, avatarURl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarURl}
+        width={40}
+        height={40}
+        alt=""
+        className="rounded-ful h-10 w-10"
+      />
+
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
